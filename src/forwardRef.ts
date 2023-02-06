5fe1601c56473082c91d5993c8a8b055881eece4
nooooooom <nooooooom@163.com>
2023-02-06 13:52:09 +0800

    feat: supports coexistence with the original ref

--- src/forwardRef.ts
+++ src/forwardRef.ts
@@ -1,7 +1,7 @@
-import { getCurrentInstance, h, VNode } from 'vue'
+import { getCurrentInstance, h, isRef, VNode } from 'vue'
 import { setRef } from './createForwardRef'
 import { ComponentInternalInstance } from './types'
-import { getVNode, getVNodeRef } from './utils'
+import { getVNode, getVNodeRef, isFunction, isString, isVue2, setVNodeRef } from './utils'
 
 type ComponentType = typeof h extends (type: infer T, ...args: any[]) => any ? T | VNode : never
 
@@ -17,17 +17,42 @@ export function forwardRef(component: ComponentType, instance = getCurrentInstan
 }
 
 function createInnerComponent(component: any, parent: ComponentInternalInstance) {
+  if (component === undefined || component === null) {
+    return
+  }
+
   const parentVNode = getVNode(parent)
   let oldRawRef: any = null
-  const vnode = h(component, {
-    ref: (refValue) => {
-      new Promise<void>((resolve) => resolve()).then(() => {
-        const rawRef = getVNodeRef(parentVNode)
-        setRef(rawRef, oldRawRef, refValue, vnode)
-        oldRawRef = rawRef
-      })
-    }
-  })
+
+  const doSet = (refValue: any) => {
+    new Promise<void>((resolve) => resolve()).then(() => {
+      const rawRef = getVNodeRef(parentVNode)
+      setRef(rawRef, oldRawRef, refValue, vnode)
+      oldRawRef = rawRef
+    })
+  }
+
+  const vnode = h(component)
+  const rawRef = getVNodeRef(vnode)
+  if (rawRef) {
+    setVNodeRef(
+      vnode,
+      normalizeRef((refValue: any) => {
+        setRef(rawRef, null, refValue, vnode)
+        doSet(refValue)
+      }, parent)
+    )
+  } else {
+    setVNodeRef(vnode, normalizeRef(doSet, parent))
+  }
 
   return vnode
+}
+
+function normalizeRef(ref: any, instance: ComponentInternalInstance) {
+  return ref != null
+    ? isString(ref) || isRef(ref) || isFunction(ref)
+      ? { i: instance, r: ref, k: undefined, f: false }
+      : ref
+    : null
 }
